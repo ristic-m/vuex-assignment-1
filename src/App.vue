a5ace70149ca04471f1f84a904ae56f5c7221de2
ristic-m <risticm@icloud.com>
2023-10-11 11:08:19 +0200

    upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        data() {
+            return {
+            count: 0
+            };
+        },
+        methods: {
+            increaseCount() {
+              this.count++;
+            },
+            decreaseCount() {
+              this.count--;
+            },
+            doubleCount() {
+              this.count*=2
+            }
+        }
+    };
+
+</script>
+
+<template>
+    <p>Counter: {{ count }}</p>
+    <button @click="decreaseCount">Decrease</button>
+    <button @click="increaseCount">Increase</button>
+    <button @click="doubleCount">Double</button>
+</template>
