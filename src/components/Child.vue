cd89758e0e0aba9db2845ad3063c1a0b4fe26c97
Nikola <nlukic97@gmail.com>
2023-10-15 22:52:36 +0200

    installing and configuring vuex, removing unecessary files/folders/code, and making updates along with adding comments

---
+++ src/components/Child.vue
@@ -0,0 +1,11 @@
+<!-- Example child component -->
+<template>
+  <div style="border:1px solid red">
+    <h1>Child component that has access to the vuex store.</h1>
+    <h2>Count: {{ this.$store.state.count }}</h2>
+    <button @click="this.$store.commit('increaseCount')">Increment from child component</button>
+  </div>
+</template>
+
+<!-- methods from store are globally accessible from child at any level- no imports necessary -->
+<!-- Docs I followed: https://vuex.vuejs.org/guide/#the-simplest-store -->
--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createStore } from 'vuex' // import the module "createStore" from vuex (which we installed)
+
+// creating the store to be used.
+const store = createStore({
+	state () {
+		return {
+			count: 0
+		}
+	},
+	mutations: {
+		increaseCount (state) {
+			state.count++;
+		},
+		decreaseCount (state) {
+			state.count--;
+		},
+		doubleCount (state) {
+			state.count*=2
+		}
+	}
+})
+
+
+const app = createApp(App)
+
+// install and add 'store' instance as a plugin to the app:
+app.use(store)
+
+app.mount('#app')
